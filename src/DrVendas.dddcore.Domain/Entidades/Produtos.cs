96d58545173b36efbaa41eb6e8430800820ae3db
Marco Sena <msena2210@gmail.com>
2019-01-07 22:27:33 -0300

    Incluído a validação em Produtos

--- src/DrVendas.dddcore.Domain/Entidades/Produtos.cs
+++ src/DrVendas.dddcore.Domain/Entidades/Produtos.cs
@@ -1,4 +1,6 @@
 ﻿using DrVendas.dddcore.Domain.Shared.Entidades;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DrVendas.dddcore.Domain.Entidades
 {
@@ -11,9 +13,38 @@ namespace DrVendas.dddcore.Domain.Entidades
         public string Unidade { get; set; }
         public int IdFornecedor { get; set; }
 
+ 
         public override bool EstaConsistente()
         {
-            throw new System.NotImplementedException();
+            ApelidoDeveSerPreenchido();
+            ApelidoDeveTerUmTamanhoLimite();
+            ValorDeverSerSuperiorAZero();
+            UnidadeDeveSerValida();
+            return !ListaErros.Any();
         }
+
+        private void ApelidoDeveSerPreenchido()
+        {
+            if (string.IsNullOrEmpty(Apelido)) ListaErros.Add("O campo apelido deve ser preenchido!");
+        }
+
+        private void ApelidoDeveTerUmTamanhoLimite()
+        {
+            if (Apelido.Length > 20) ListaErros.Add("O campo apelido deve ter no máximo 20 caracteres!");
+        }
+
+        private void ValorDeverSerSuperiorAZero()
+        {
+            if (Valor <= 0) ListaErros.Add("O campo valor dever ser maior que zero!");
+        }
+
+
+        private void UnidadeDeveSerValida()
+        {
+            var listunidade = new List<string> { "KL", "GR", "MT", "CM", "QT" };
+            if (!listunidade.Contains(Unidade)) ListaErros.Add("Unidade deve ser KL, GR, MT, CM ou QT!");
+        }
+
+
     }
 }
