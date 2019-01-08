601cd1061d5e1c5bcb139a2897bc34e14c91f785
Marco Sena <msena2210@gmail.com>
2019-01-08 10:18:54 -0300

    Incluído a validação em Pedidos e alterado Produtos

--- src/DrVendas.dddcore.Domain/Entidades/Pedidos.cs
+++ src/DrVendas.dddcore.Domain/Entidades/Pedidos.cs
@@ -9,7 +9,7 @@ namespace DrVendas.dddcore.Domain.Entidades
         public string Apelido { get; set; }
         public string Nome { get; set; }
         public DateTime Data { get; set; }
-        public DateTime DataEntrega { get; set; }
+        public DateTime? DataEntrega { get; set; }
         public int Status { get; set; }
         public int IdCliente { get; set; }
 
--- src/DrVendas.dddcore.Domain/Entidades/Produtos.cs
+++ src/DrVendas.dddcore.Domain/Entidades/Produtos.cs
@@ -9,6 +9,7 @@ namespace DrVendas.dddcore.Domain.Entidades
        
         public string Apelido { get; set; }
         public string Nome { get; set; }
+        public string Descricao { get; set; }
         public decimal Valor { get; set; }
         public string Unidade { get; set; }
         public int IdFornecedor { get; set; }
@@ -18,8 +19,13 @@ namespace DrVendas.dddcore.Domain.Entidades
         {
             ApelidoDeveSerPreenchido();
             ApelidoDeveTerUmTamanhoLimite();
+            NomeDeveSerPreenchido();
+            NomeDeveTerUmTamanhoLimite();
+            DescricaoDeveSerPreenchido();
+            DescricaoDeveTerUmTamanhoLimite();
             ValorDeverSerSuperiorAZero();
             UnidadeDeveSerValida();
+            FornecedorDeveSerPreenchido();
             return !ListaErros.Any();
         }
 
@@ -33,6 +39,27 @@ namespace DrVendas.dddcore.Domain.Entidades
             if (Apelido.Length > 20) ListaErros.Add("O campo apelido deve ter no máximo 20 caracteres!");
         }
 
+        private void NomeDeveSerPreenchido()
+        {
+            if (string.IsNullOrEmpty(Nome)) ListaErros.Add("O campo nome deve ser preenchido!");
+        }
+
+        private void NomeDeveTerUmTamanhoLimite()
+        {
+            if (Nome.Length > 50) ListaErros.Add("O campo nome deve ter no máximo 50 caracteres!");
+        }
+
+        private void DescricaoDeveSerPreenchido()
+        {
+            if (string.IsNullOrEmpty(Descricao)) ListaErros.Add("O campo descrição deve ser preenchido!");
+        }
+
+        private void DescricaoDeveTerUmTamanhoLimite()
+        {
+            if (Descricao.Length > 150) ListaErros.Add("O campo descrição deve ter no máximo 150 caracteres!");
+        }
+
+
         private void ValorDeverSerSuperiorAZero()
         {
             if (Valor <= 0) ListaErros.Add("O campo valor dever ser maior que zero!");
@@ -45,6 +72,10 @@ namespace DrVendas.dddcore.Domain.Entidades
             if (!listunidade.Contains(Unidade)) ListaErros.Add("Unidade deve ser KL, GR, MT, CM ou QT!");
         }
 
+        private void FornecedorDeveSerPreenchido()
+        {
+            if (IdFornecedor == 0) ListaErros.Add("O campo fornecedor dever ser informado!");
+        }
 
     }
 }
