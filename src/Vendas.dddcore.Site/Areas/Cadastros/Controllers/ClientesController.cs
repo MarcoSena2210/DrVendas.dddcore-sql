17dd2d116a534ea7b4050fe52278140c20804b14
Marco Sena <msena2210@gmail.com>
2019-02-24 18:52:32 -0300

    Criado o Controller Fornecedores e as telas de navegação aula38

--- src/Vendas.dddcore.Site/Areas/Cadastros/Controllers/ClientesController.cs
+++ src/Vendas.dddcore.Site/Areas/Cadastros/Controllers/ClientesController.cs
@@ -9,8 +9,10 @@ namespace Vendas.dddcore.Site.Areas.Cadastros.Controllers
     [Area("Cadastros")]
     public class ClientesController : CadastroBaseController
     {
+
         /*Para usar o obter todos da camada de aplicação precisamos fazer a injeção de dependências
-         * precisa instanciar todas as classe que precisamos, quem faz é o contanner de injecçao de dependencias tem varios
+         * precisa instanciar todas as classe que precisamos, 
+         * quem faz é o contanner de injecçao de dependencias tem varios
          * como inject, vamos usar o nativo do dotnet, na camada de cross cutting*/
 
         public readonly IApplicationCliente appClientes;
--- src/Vendas.dddcore.Site/Areas/Cadastros/Controllers/FornecedoresController.cs
+++ src/Vendas.dddcore.Site/Areas/Cadastros/Controllers/FornecedoresController.cs
@@ -1,4 +1,6 @@
 ﻿using DrVendas.dddcore.Application.AppVendas.Interfaces;
+using DrVendas.dddcore.Application.AppVendas.ViewModels;
+using DrVendas.dddcore.Application.Shared.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -7,24 +9,117 @@ namespace Vendas.dddcore.Site.Areas.Cadastros.Controllers
     [Area("Cadastros")]
     public class FornecedoresController : CadastroBaseController
     {
-        private readonly IApplicationFornecedor appfornecedor;
+        private readonly IApplicationFornecedor appFornecedores;
+        public readonly IApplicationShared appShared;
 
-        public FornecedoresController(IApplicationFornecedor _appfornecedor)
+
+        public FornecedoresController(IApplicationFornecedor _appFornecedores
+                                                         ,IApplicationShared _appShared)
         {
-            appfornecedor = _appfornecedor;
+            appFornecedores  = _appFornecedores;
+            appShared = _appShared;
         }
+
+        #region LISTAGEM FORNECEDORES
+        [Route("Cadastro-Fornecedores-Listagem")]
         public IActionResult Index()
         {
+            ViewBag.RetornoPost = TempData["RetornoPost"];  //recebe do post do excluir
             return View();
         }
 
         public JsonResult ListagemFornecedoresJson()
         {
-            var lista = appfornecedor.ObterTodos();
+            var lista = appFornecedores.ObterTodos();
             var settings = new JsonSerializerSettings();
             return Json(lista, settings);
         }
+        #endregion
 
+        #region INCLUIR-CLIENTE
+        [Route("Cadastro-Clientes-Incluir")]
+        public IActionResult Incluir()
+        {
+            //ViewBag =forma de  passar dados para view
+            ViewBag.ListaEstados = appShared.ObterEstados();
+            return View();
+        }
+
+        [Route("Cadastro-Fornecedores-Incluir")]
+        [HttpPost]
+        public IActionResult Incluir(FornecedorViewModel model)
+        {
+            ViewBag.ListaEstados = appShared.ObterEstados();
+            if (!ModelState.IsValid) return View();  //verifica se passou por todas validações  ou model
+            var fornecedor = appFornecedores.Adicionar(model);
+            ViewBag.RetornoPost = "success,Fornecedor incluído com sucesso!";
+            if (VerificaErros(fornecedor.ListaErros))
+            {
+                ViewBag.RetornoPost = "error,Não foi possível incluir o fornecedor!";
+            }
+            return View(model);
+        }
+        #endregion FIM-INCLUIR-CLIENTE
+
+        #region ALTERAR-FORNECEDOR
+
+        [Route("Cadastro-Fornecedores-Alterar")]
+        public IActionResult Alterar(int id)
+        {
+            ViewBag.ListaEstados = appShared.ObterEstados();
+            var model = appFornecedores.ObterPorId(id);
+            return View(model);
+        }
+
+        [Route("Cadastro-Fornecedores-Alterar")]
+        [HttpPost]
+        public IActionResult Alterar(FornecedorViewModel model)
+        {
+            ViewBag.ListaEstados = appShared.ObterEstados();
+            if (!ModelState.IsValid) return View();
+            var fornecedor = appFornecedores.Atualizar(model);
+            ViewBag.RetornoPost = "success,Fornecedor alterado com sucesso!";
+            if (VerificaErros(fornecedor.ListaErros))
+            {
+                ViewBag.RetornoPost = "error,Não foi possível alterar o fornecedor!";
+            }
+            return View(model);
+        }
+        #endregion FIM-ALTERAR-CLIENTE
+
+        #region DETALHAR-FORNECEDOR
+        [Route("Cadastro-Fornecedores-Detalhar")]
+        public IActionResult Detalhar(int id)
+        {
+            var model = appFornecedores.ObterPorId(id);
+            return View(model);
+        }
+        #endregion FIM-DETALHAR-CLIENTE
+
+        #region EXCLUIR-FORNECEDOR
+        [Route("Cadastro-Fornecedor-Excluir")]
+        public IActionResult Excluir(int id)
+        {
+            var model = appFornecedores.ObterPorId(id);
+            return View(model);
+        }
+
+        [Route("Cadastro-Fornecedores-Excluir")]
+        [HttpPost]
+        public IActionResult Excluir(FornecedorViewModel model)
+        {
+            // TempData, guarda a mensagem para ser exibido na pagina INDEX 
+            //se houver 
+            var fornecedor = appFornecedores.Remover(model);
+            TempData["RetornoPost"] = "success,Fornecedor excluído com sucesso!";
+            if (VerificaErros(fornecedor.ListaErros))
+            {
+                ViewBag.RetornoPost = "error,Não foi possível excluir o fornecedor!";
+                return View(model);
+            }
+            return RedirectToAction("Index");
+        }
+        #endregion FIM-EXCLUIR-FORNECEDOR
 
     }
 }
